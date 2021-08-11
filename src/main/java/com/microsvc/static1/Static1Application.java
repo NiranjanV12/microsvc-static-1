4aa95220227b954940245a5aae277352c5d32f49
niranjanv12 <niranjanvaity@gmail.com>
2021-08-11 18:03:48 +0530

    first commit

---
+++ src/main/java/com/microsvc/static1/Static1Application.java
@@ -0,0 +1,13 @@
+package com.microsvc.static1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Static1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Static1Application.class, args);
+	}
+
+}
