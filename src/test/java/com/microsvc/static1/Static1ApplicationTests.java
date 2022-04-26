caa11107ae9515f1ccf4695e0776b3998689bc46
niranjanv12 <niranjanvaity@gmail.com>
2022-04-26 13:18:43 +0530

    tests

---
+++ src/test/java/com/microsvc/static1/Static1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsvc.static1;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class Static1ApplicationTests {
+    @Test
+    public void tempTEST() throws Exception {
+        assertThat(true).isEqualTo(true);
+    }
+    
+}
